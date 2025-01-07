6410cf3dc0c94a1423ba7c6b16414bb6b2ba3cc2
Pedro Araujo <ing.pedroaraujo@gmail.com>
2025-01-07 07:04:15 -0500

    crear y leer datos de mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import { envs } from "./config/plugins/envs.plugins";
-import { MongoDatabase } from "./data/mongo";
+import { LogModel, MongoDatabase } from "./data/mongo";
 
 import { Server } from "./presentation/server";
 
@@ -17,6 +17,22 @@ async function main(){
         mongoUrl: envs.MONGO_URL,
         mongoDBName: envs.MONGO_DB_NAME 
     });
+
+    /// crear una coleccion = tables, documento = registro
+   /*  const newLog = await LogModel.create({
+        message: 'Test Message from Mongo',
+        origin: 'app.ts',
+        level:  'low'
+
+    });
+ 
+    await newLog.save();
+    console.log(newLog);  */
+
+    const logs = await LogModel.find();
+    console.log(logs);
+
+
 
     // console.log( envs );
     // Server.start();
